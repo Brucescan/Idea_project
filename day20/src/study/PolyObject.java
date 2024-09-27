146a1f4b1d6d894b7544504dfe62b3523dfcd5ea
Brucescan <1637252023@qq.com>
2024-09-27 19:02:29 +0800

    day20_2

--- day20/src/study/PolyObject.java
+++ day20/src/study/PolyObject.java
@@ -1,4 +1,5 @@
 package study;
+
 //理解起来较为困难
 //对象的多态
 //1、一个对象的编译类型和运行类型可以不一致
@@ -12,26 +13,45 @@ public class PolyObject {
         //因为运行时，这时(执行到第九句话时),animal运行类型是dog，所以这个cry就是dog的cry
         animal.cry();
         //此时animal这里的编译类型还是animal，运行类型是Cat
+        //因为运行是在编译之后的
         animal = new Cat();//animal 的运行类型变成了Cat，编译类型仍然是Animal，所以这个cry就是cat的cry
         animal.cry();
+        animal.sleep();
+//        animal.catchMouse();//错误的，因为因为在编译阶段，可以调用哪些，是由编译类型决定的
+
+        //如果我们想要调用，可以使用多态的向下转型,将父类的引用强行转换成子类的引用
+        Cat cat1 = (Cat) animal;
+        //我们可以发现，现在已经可以调用子类类型中的所有成员
+        //这时cat1的编译类型是Cat 运行类型也是Cat
+        cat1.catchMouse();
+        cat1.cry();
+        cat1.sleep();
+        //原先的那个animal还是存在的 
     }
 }
 
-class Animal{
-    public void cry(){
+class Animal {
+    public void cry() {
         System.out.println("动物叫");
     }
+
+    public void sleep() {
+        System.out.println("睡大觉");
+    }
 }
 
-class Dog extends Animal{
-    public void cry(){
+class Dog extends Animal {
+    public void cry() {
         System.out.println("小狗叫");
     }
 }
 
-class Cat extends Animal{
-    public void cry(){
+class Cat extends Animal {
+    public void cry() {
         System.out.println("小猫叫");
+    }
+    public void catchMouse(){
+        System.out.println("抓老鼠");
     }
 
 }
