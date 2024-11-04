933f61855e6116042fba15a8c6a9b5488980c355
Brucescan <1637252023@qq.com>
2024-11-04 15:31:40 +0800

    day41

---
+++ day41/src/tankgame/TankGame03.java
@@ -0,0 +1,25 @@
+package tankgame;
+
+import javax.swing.*;
+
+/**
+ * @author bruces
+ * @version 1.0
+ */
+public class TankGame01 extends JFrame {
+    //定义一个MyPannel
+    MyPanel mp = null;
+    public static void main(String[] args) {
+        new TankGame01();
+    }
+
+    public TankGame01() {
+        mp = new MyPanel();
+        this.add(mp);//这个面板就是游戏的绘图区域
+        this.setSize(1000,750);
+        //添加事件监听
+        this.addKeyListener(mp);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
